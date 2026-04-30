8bc07401b01e80f53ae410101ad25597155c5cac
Yash Kakade <yashkakade333@gmail.com>
2026-04-30 13:39:50 +0530

    Final fix - dynamic port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
@@ -33,5 +34,6 @@ def predict():
             "error": str(e)
         }), 500
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
